aaa719a1d99fd1ecfafbe3dce4c4fedf73b29470
Oluwatobiloba Oluwadare <tobidechamp15@gmail.com>
2026-04-29 11:12:31 +0100

    main changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Include if using the App Directory feature in Next.js 13+
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
-        logo: ['Ink Free', 'sans-serif'],
-        nav: ['Corbel', 'sans'],
-        tiny: ['Verdana', 'sans-serif']
+      backgroundImage: {
+        "hero-bg": "url('/assets/hero-bg.svg')",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors: {
-        grad: ['rgba(0, 0, 0, 0.6)']
-      }
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+        cursive: ["Oleo Script", "cursive"],
+      },
+    },
+    screens: {
+      xs: { max: "639px" },
+      xsm: { max: "767px" },
+      ...defaultTheme.screens,
     },
   },
   plugins: [],
